f7efae1ea5ace1f3743be038e17a4e3d403fbc75
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2023-09-09 05:24:47 -0400

    cleanup

--- scaffold.js
+++ scaffold.js
@@ -15,34 +15,29 @@ export async function scaffold({
 	deploymentTypes,
 }) {
 	const type = await logger.listInput("Select deployment type", [
-		deploymentTypes.map((t) => ({
-			name: t,
-			value: t,
-		})),
+		["none"],
+		deploymentTypes,
 	]);
-	if (type != null) {
-		const deployInfo = await generateDeploymentInfo(null, {
-			deployment: { type },
-		});
-		const deploymentRootPath = path.resolve(
-			projectRootPath,
-			"deployments",
-			deployInfo.name,
-		);
-		logger.info(`Scaffolding ${deployInfo.name} at ${deploymentRootPath}`);
-		await fs.copy(sourceDirectory, deploymentRootPath);
-		const packageJSON = JSON.parse(
-			await fs.readFile(
-				path.resolve(deploymentRootPath, "package.json"),
-				"utf8",
-			),
-		);
-		packageJSON.name = `@${projectConfig.name}/${deployInfo.name}`;
-		await fs.writeFile(
-			path.resolve(deploymentRootPath, "package.json"),
-			JSON.stringify(packageJSON, null, "\t"),
-		);
-	} else {
-		throw new Error("No deployment type selected");
-	}
+	const deployInfo = await generateDeploymentInfo(null, {
+		...(type != null && {
+			deployment: {
+				type,
+			},
+		}),
+	});
+	const deploymentRootPath = path.resolve(
+		projectRootPath,
+		"deployments",
+		deployInfo.name,
+	);
+	logger.info(`Scaffolding ${deployInfo.name} at ${deploymentRootPath}`);
+	await fs.copy(sourceDirectory, deploymentRootPath);
+	const packageJSON = JSON.parse(
+		await fs.readFile(path.resolve(deploymentRootPath, "package.json"), "utf8"),
+	);
+	packageJSON.name = `@${projectConfig.name}/${deployInfo.name}`;
+	await fs.writeFile(
+		path.resolve(deploymentRootPath, "package.json"),
+		JSON.stringify(packageJSON, null, "\t"),
+	);
 }
